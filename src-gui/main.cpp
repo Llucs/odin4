55a1225ddc8bdda9828e644b881160485535a089
Llucs <104036495+Llucs@users.noreply.github.com>
2026-05-13 02:57:13 +0000

    Apply clang-format

--- src-gui/main.cpp
+++ src-gui/main.cpp
@@ -18,42 +18,44 @@
 
 class FlashWorker : public QObject {
     Q_OBJECT
-public:
-    FlashWorker(const OdinConfig &cfg) : m_cfg(cfg) {}
+  public:
+    FlashWorker(const OdinConfig& cfg) : m_cfg(cfg) {}
 
-public slots:
+  public slots:
     void process() {
         odin4_init(m_cfg);
         OdinExitCode result = odin4_run(m_cfg);
         emit finished(result);
     }
 
-signals:
+  signals:
     void finished(OdinExitCode result);
 
-private:
+  private:
     OdinConfig m_cfg;
 };
 
 class OdinGui : public QMainWindow {
     Q_OBJECT
-public:
-    OdinGui(QWidget *parent = nullptr) : QMainWindow(parent), flashThread(nullptr) {
+  public:
+    OdinGui(QWidget* parent = nullptr) : QMainWindow(parent), flashThread(nullptr) {
         setWindowTitle("Odin4 GUI - Llucs");
         setMinimumSize(700, 500);
 
-        auto *centralWidget = new QWidget(this);
-        auto *layout = new QVBoxLayout(centralWidget);
+        auto* centralWidget = new QWidget(this);
+        auto* layout = new QVBoxLayout(centralWidget);
 
-        auto createBrowseRow = [&](const QString &label, QLineEdit *&edit) {
-            auto *row = new QHBoxLayout();
+        auto createBrowseRow = [&](const QString& label, QLineEdit*& edit) {
+            auto* row = new QHBoxLayout();
             row->addWidget(new QLabel(label + ":"), 0);
             edit = new QLineEdit();
             row->addWidget(edit, 1);
-            auto *btn = new QPushButton("Browse");
+            auto* btn = new QPushButton("Browse");
             connect(btn, &QPushButton::clicked, [this, edit]() {
-                QString file = QFileDialog::getOpenFileName(this, "Select File", "", "Samsung Firmware (*.tar *.tar.md5 *.lz4 *.bin)");
-                if (!file.isEmpty()) edit->setText(file);
+                QString file = QFileDialog::getOpenFileName(this, "Select File", "",
+                                                            "Samsung Firmware (*.tar *.tar.md5 *.lz4 *.bin)");
+                if (!file.isEmpty())
+                    edit->setText(file);
             });
             row->addWidget(btn, 0);
             layout->addLayout(row);
@@ -65,11 +67,11 @@ public:
         createBrowseRow("CSC", editCSC);
         createBrowseRow("UMS", editUMS);
 
-        auto *devRow = new QHBoxLayout();
+        auto* devRow = new QHBoxLayout();
         devRow->addWidget(new QLabel("Device:"));
         comboDevices = new QComboBox();
         devRow->addWidget(comboDevices, 1);
-        auto *btnRefresh = new QPushButton("Refresh");
+        auto* btnRefresh = new QPushButton("Refresh");
         connect(btnRefresh, &QPushButton::clicked, this, &OdinGui::refreshDevices);
         devRow->addWidget(btnRefresh);
         layout->addLayout(devRow);
@@ -82,7 +84,7 @@ public:
         progressBar = new QProgressBar();
         layout->addWidget(progressBar);
 
-        auto *btnRow = new QHBoxLayout();
+        auto* btnRow = new QHBoxLayout();
         btnStart = new QPushButton("Start Flash");
         btnStart->setMinimumHeight(40);
         connect(btnStart, &QPushButton::clicked, this, &OdinGui::startFlash);
@@ -90,34 +92,39 @@ public:
         layout->addLayout(btnRow);
 
         setCentralWidget(centralWidget);
-        
+
         odin4_set_log_callback([](int level, const char* message) {
             QString msg = QString("[%1] %2").arg(level).arg(message);
-            QMetaObject::invokeMethod(qApp, [msg]() {
-                for (auto *widget : qApp->topLevelWidgets()) {
-                    if (auto *win = qobject_cast<OdinGui*>(widget)) {
-                        win->appendLog(msg);
-                        return;
+            QMetaObject::invokeMethod(
+                qApp,
+                [msg]() {
+                    for (auto* widget : qApp->topLevelWidgets()) {
+                        if (auto* win = qobject_cast<OdinGui*>(widget)) {
+                            win->appendLog(msg);
+                            return;
+                        }
                     }
-                }
-                // Fallback to active window if OdinGui instance not found in topLevelWidgets
-                if (auto *win = qobject_cast<OdinGui*>(qApp->activeWindow())) {
-                    win->appendLog(msg);
-                }
-            }, Qt::QueuedConnection);
+                    // Fallback to active window if OdinGui instance not found in topLevelWidgets
+                    if (auto* win = qobject_cast<OdinGui*>(qApp->activeWindow())) {
+                        win->appendLog(msg);
+                    }
+                },
+                Qt::QueuedConnection);
         });
 
         refreshDevices();
     }
 
-    void appendLog(const QString &msg) {
+    void appendLog(const QString& msg) {
         logView->append(msg);
     }
 
-protected:
-    void closeEvent(QCloseEvent *event) override {
+  protected:
+    void closeEvent(QCloseEvent* event) override {
         if (flashThread && flashThread->isRunning()) {
-            auto ret = QMessageBox::question(this, "Exit", "A flash process is currently running. Are you sure you want to exit?", QMessageBox::Yes | QMessageBox::No);
+            auto ret = QMessageBox::question(this, "Exit",
+                                             "A flash process is currently running. Are you sure you want to exit?",
+                                             QMessageBox::Yes | QMessageBox::No);
             if (ret == QMessageBox::No) {
                 event->ignore();
                 return;
@@ -128,15 +135,16 @@ protected:
         event->accept();
     }
 
-private slots:
+  private slots:
     void refreshDevices() {
         comboDevices->clear();
         OdinConfig cfg;
         auto devices = odin4_list_devices(cfg);
-        for (const auto &dev : devices) {
+        for (const auto& dev : devices) {
             comboDevices->addItem(QString::fromStdString(dev));
         }
-        if (devices.empty()) comboDevices->addItem("No device detected");
+        if (devices.empty())
+            comboDevices->addItem("No device detected");
     }
 
     void startFlash() {
@@ -164,7 +172,7 @@ private slots:
         progressBar->setRange(0, 0);
 
         flashThread = new QThread(this);
-        auto *worker = new FlashWorker(cfg);
+        auto* worker = new FlashWorker(cfg);
         worker->moveToThread(flashThread);
 
         connect(flashThread, &QThread::started, worker, &FlashWorker::process);
@@ -183,22 +191,23 @@ private slots:
             logView->append("<b>Flash successful!</b>");
             progressBar->setValue(100);
         } else {
-            logView->append(QString("<span style='color:red;'>Flash failed with code: %1</span>").arg(static_cast<int>(result)));
+            logView->append(
+                QString("<span style='color:red;'>Flash failed with code: %1</span>").arg(static_cast<int>(result)));
             progressBar->setValue(0);
         }
         flashThread = nullptr;
     }
 
-private:
+  private:
     QLineEdit *editBL, *editAP, *editCP, *editCSC, *editUMS;
-    QComboBox *comboDevices;
-    QTextEdit *logView;
-    QProgressBar *progressBar;
-    QPushButton *btnStart;
-    QThread *flashThread;
+    QComboBox* comboDevices;
+    QTextEdit* logView;
+    QProgressBar* progressBar;
+    QPushButton* btnStart;
+    QThread* flashThread;
 };
 
-int main(int argc, char *argv[]) {
+int main(int argc, char* argv[]) {
     QApplication app(argc, argv);
     OdinGui gui;
     gui.show();
