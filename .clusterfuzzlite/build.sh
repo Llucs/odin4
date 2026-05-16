e9822fdbe25e1b1989b9b95091a210ff21b16c72
Llucs <104036495+Llucs@users.noreply.github.com>
2026-05-15 23:30:06 -0300

    Atualizar o build.sh

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 set -euxo pipefail
 
+export CC=clang
+export CXX=clang++
+
+export CXXFLAGS="-O1 -g -std=c++23 -stdlib=libc++"
+export CFLAGS="-O1 -g"
+export LDFLAGS="-stdlib=libc++"
+
 apt-get update || true
 apt-get install -y --no-install-recommends \
     cmake \
@@ -10,22 +17,26 @@ apt-get install -y --no-install-recommends \
     libusb-1.0-0-dev \
     libcrypto++-dev \
     libarchive-dev \
-    zlib1g-dev || true
+    zlib1g-dev \
+    libc++-dev \
+    libc++abi-dev || true
 
 mkdir -p build
 cd build
 
-cmake .. -G Ninja -DCMAKE_BUILD_TYPE=Release \
+cmake .. -G Ninja \
+    -DCMAKE_BUILD_TYPE=Release \
     -DODIN4_BUILD_GUI=OFF \
     -DODIN4_BUILD_TESTS=OFF \
     -DCMAKE_C_FLAGS="-O1 -g" \
-    -DCMAKE_CXX_FLAGS="-O1 -g"
+    -DCMAKE_CXX_FLAGS="-O1 -g -std=c++23 -stdlib=libc++"
 
 cmake --build . --parallel
 
+mkdir -p "$OUT"
+
 $CXX $CXXFLAGS \
     -fsanitize=fuzzer,address,undefined \
-    -std=c++23 \
     -I../include \
     ../tests/fuzz_pit.cpp \
     -L. \
@@ -33,7 +44,6 @@ $CXX $CXXFLAGS \
 
 $CXX $CXXFLAGS \
     -fsanitize=fuzzer,address,undefined \
-    -std=c++23 \
     -I../include \
     ../tests/fuzz_thor.cpp \
     -L. \
@@ -41,7 +51,6 @@ $CXX $CXXFLAGS \
 
 $CXX $CXXFLAGS \
     -fsanitize=fuzzer,address,undefined \
-    -std=c++23 \
     -I../include \
     ../tests/fuzz_lz4.cpp \
     -L. \
@@ -49,7 +58,6 @@ $CXX $CXXFLAGS \
 
 $CXX $CXXFLAGS \
     -fsanitize=fuzzer,address,undefined \
-    -std=c++23 \
     -I../include \
     ../tests/fuzz_tar.cpp \
     -L. \
