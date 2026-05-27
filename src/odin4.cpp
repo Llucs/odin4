9d58091cd10da4f0789d7275374264cd48ec6706
Llucs <104036495+Llucs@users.noreply.github.com>
2026-05-26 21:53:27 -0300

    Atualizar o odin4.cpp

--- src/odin4.cpp
+++ src/odin4.cpp
@@ -28,7 +28,7 @@
 #include <format>
 #include <filesystem>
 
-#define ODIN4_VERSION "7.0.0-915a353"
+#define ODIN4_VERSION "7.1.0-9b9be08"
 
 auto odin4_get_version() -> const char* {
     return ODIN4_VERSION;
