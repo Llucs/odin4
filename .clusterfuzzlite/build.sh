c7d84e6170c9956a859999bb1dea0ebd399704b3
Llucs <c307lucas@gmail.com>
2026-06-07 19:55:40 +0000

    Fix CFLite DWARF: use lld linker and -Xclang dwarf-version=4

--- .clusterfuzzlite/build.sh
+++ .clusterfuzzlite/build.sh
@@ -4,8 +4,8 @@ set -euxo pipefail
 export CC=clang
 export CXX=clang++
 
-export CXXFLAGS="-O1 -g -gdwarf-4 -std=c++23 -stdlib=libc++ -pthread"
-export CFLAGS="-O1 -g -gdwarf-4 -pthread"
+export CXXFLAGS="-O1 -g -std=c++23 -stdlib=libc++ -pthread"
+export CFLAGS="-O1 -g -pthread"
 
 apt-get update || true
 apt-get install -y --no-install-recommends \
@@ -18,7 +18,8 @@ apt-get install -y --no-install-recommends \
     libusb-1.0-0-dev \
     libcrypto++-dev \
     libarchive-dev \
-    zlib1g-dev || true
+    zlib1g-dev \
+    lld || true
 
 mkdir -p build
 cd build
@@ -27,9 +28,9 @@ cmake .. -G Ninja \
     -DCMAKE_BUILD_TYPE=Release \
     -DODIN4_BUILD_GUI=OFF \
     -DODIN4_BUILD_TESTS=OFF \
-    -DCMAKE_C_FLAGS="-O1 -g -gdwarf-4" \
-    -DCMAKE_CXX_FLAGS="-O1 -g -gdwarf-4 -std=c++23 -stdlib=libc++" \
-    -DCMAKE_EXE_LINKER_FLAGS="-pthread"
+    -DCMAKE_C_FLAGS="-O1 -g" \
+    -DCMAKE_CXX_FLAGS="-O1 -g -std=c++23 -stdlib=libc++" \
+    -DCMAKE_EXE_LINKER_FLAGS="-pthread -fuse-ld=lld"
 
 cmake --build . --parallel
 
@@ -40,17 +41,21 @@ INCLUDES="-I../include -I../src"
 FUZZ_UTILS="../src/fuzz_utils.cpp"
 
 $CXX $CXXFLAGS -fsanitize=fuzzer,address,undefined \
+    -Xclang -dwarf-version=4 \
     $INCLUDES ../tests/fuzz_pit.cpp $FUZZ_UTILS \
     -o $OUT/fuzz_pit
 
 $CXX $CXXFLAGS -fsanitize=fuzzer,address,undefined \
+    -Xclang -dwarf-version=4 \
     $INCLUDES ../tests/fuzz_thor.cpp $FUZZ_UTILS \
     -o $OUT/fuzz_thor
 
 $CXX $CXXFLAGS -fsanitize=fuzzer,address,undefined \
+    -Xclang -dwarf-version=4 \
     $INCLUDES ../tests/fuzz_lz4.cpp \
     -o $OUT/fuzz_lz4
 
 $CXX $CXXFLAGS -fsanitize=fuzzer,address,undefined \
+    -Xclang -dwarf-version=4 \
     $INCLUDES ../tests/fuzz_tar.cpp \
     -o $OUT/fuzz_tar
